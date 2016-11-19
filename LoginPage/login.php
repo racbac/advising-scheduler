e33de9068bb5348985292ad43666bef7436bd291
cganley1 <cganley1@umbc.edu>
2016-11-18 20:43:58 -0500

    first login page changes

--- LoginPage/login.php
+++ LoginPage/login.php
@@ -11,34 +11,35 @@ session_start();
 
 <html>
 <head>
-<title></title>
+  <title>Login</title>
 </head>
 <body>
 
-<?php
-   if($_SESSION['userValue'] == true)
-{
-   echo "Invalid username/password combination.";
-}
-$_SESSION['advisorExists'] = false;
-$_SESSION['confirmedPass'] = false;
-?>
+  <?php
+   if($_SESSION['userToken'] == false) { echo "Invalid username/password combination."; }
+   $_SESSION['advisorExists'] = false;
+   $_SESSION['confirmedPass'] = false;
+   ?>
 
-<form action='processLogin.php' method='post' name='AdvisorLogin'>
+<form action='loginAuth.php' method='post' name='AdvisorLogin'>
   <div class='field'>
-    <label for='username'>Username</label><br/>
-    <input id='username' type='text' size='25' maxlength='50' name='username' required><br/>
+    <label for='username'>Username:</label>
+    <input id='username' type='text' name='username' required>
   </div>
 
   <div class='field'>
-    <label for='passw'>Password</label><br/>
-    <input id='pass' type='password' size='25' maxlength='50' name='pass' required><br/>
+    <label for='password'>Password</label>
+    <input id='password' type='password' name='pass' required>
   </div>
 
   <div class='loginButton'>
     <input type='submit' value='Login'>
   </div>
 </form>
+
+
+
+
 
 <form action='advisorInfo.php' method='post' name='CreateAccount'>
   <div class='field'>
