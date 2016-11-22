e0ef4d022743c98d7e1ab921a77d825950693bf8
cganley1 <cganley1@umbc.edu>
2016-11-22 10:46:17 -0500

    updated common methods

--- CommonMethods.php
+++ CommonMethods.php
@@ -5,38 +5,30 @@
    Email: fbastos1@umbc.edu, bac2@umbc.edu, te4@umbc.edu, fullern1@umbc.edu, cganley1@umbc.edu
 -->
 
-<?php 
-
-class Common
-{     
+<?php
+class Common {    
    var $conn;
    var $debug;
-		
-	function Common($debug)
-	{
-	$this->debug = $debug; 
-	$rs = $this->connect("XXXXXXX"); // db name really here
-	return $rs;
-	}
 
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-   
-   function connect($db)// connect to MySQL
-   {
-	$conn = @mysql_connect("studentdb-maria.gl.umbc.edu", "cganley1", "peasandcarrots") or die("Could not connect to MySQL");
-	      $rs = @mysql_select_db($db, $conn) or die("Could not connect select $db database");
-	      	  $this->conn = $conn; 
-		  }
+	//constructor
+	function Common($debug) {	
+	  $this->debug = $debug;
+	  $rs = $this->connect("cganley1"); 
+	  return $rs;
+	}	
 
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
-   
-   function executeQuery($sql, $filename) // execute query
-   {
-	if($this->debug == true) { echo("$sql <br>\n"); }
-	$rs = mysql_query($sql, $this->conn) or die("Could not execute query '$sql' in $filename"); 
-	return $rs;
-   }			
+	//opens db connection
+   	function connect($db) {
+	  $conn = @mysql_connect("studentdb-maria.gl.umbc.edu", "cganley1", "peasandcarrots") or die("Could not connect to MySQL")
+	  $rs = @mysql_select_db($db, $conn) or die("Could not connect select $db database");
+	  $this->conn = $conn;
+	} 
 
-} // ends class, NEEDED!!
-
+	//executes a given query
+   	function executeQuery($sql, $filename) {
+	  if($this->debug == true) { echo("$sql <br>\n"); }
+	  $rs = mysql_query($sql, $this->conn) or die("Could not execute query '$sql' in $filename");
+	  return $rs;
+   	}
+} 
 ?>
