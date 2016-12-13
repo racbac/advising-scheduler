a05639a7096390c5a4c47d925e281e992e9d3732
travisfe <travis.earley96@gmail.com>
2016-12-13 10:31:19 -0500

    Travis (#13)
    * Updated files to work with new Architecture
    
    * fixed bugs

--- AdvisorManager/advisorInfo.php
+++ AdvisorManager/advisorInfo.php
@@ -4,28 +4,19 @@
    Date: 2016-12-13
    Email: fbastos1@umbc.edu, bac2@umbc.edu, te4@umbc.edu, fullern1@umbc.edu, cganley1@umbc.edu
 -->
-
 <?php
+
 session_start();
+
+//if(!$_SESSION['userToken']) { header('Location: ../error.html'); }
+
 ?>
+
 <html>
 <head>
 <title></title>
 </head>
 <body>
-
-<?php
-if($_SESSION['confirmedPass'] == true)
-{
-   echo "Passwords do not match.";
-}
-
-if($_SESSION['advisorExists'] == true)
-  {
-    echo "Advisor already exists.";
-  }
-?>
-
 
 <form action='processNewAdvisor.php' method='post' name='NewAdvisorProfile'>
   First Name: <input type='text' size='25' maxlength='25' name='fname' required><br/>
@@ -35,17 +26,6 @@ if($_SESSION['advisorExists'] == true)
   Confirm Password: <input type='password' size='25' maxlength='50' name='confirmPass' required><br/><br/>
   Office Location: <input type='text' size='25' maxlength='10' name='office' required><br/><br/>
   Email: <input type='email' size='25' maxlength='50' name='email' required><br/><br/>
-  Select Majors to Advise:<br/>
-  <select name='majors[]' multiple='multiple'>
-  <option value='bsci_BA'>Biological Sciences BA</option>
-  <option value='bsci_BS'>Biological Sciences BS</option>
-  <option value='bchem_BS'>Biochemistry & Molecular Biology BS</option>
-  <option value='binf_BS'>Bioinformatics & Computational Biology BS</option>
-  <option value='bsciEd_BA'>Biology Education BA</option>
-  <option value='chem_BA'>Chemistry BA</option>
-  <option value='chem_BS'>Chemistry BS</option>
-  <option value='chemEd_BA'>Chemistry Education BA</option>
-  </select><br/><br/>
   <input type='submit' value='Save Profile'>
 </form>
 
