f7f4b4082e6a368f2bad6b44c9bf545746c62193
Felipe Bastos <felipe.bastos@terra.com.br>
2016-12-15 20:19:15 -0500

    Login/account creation page styling

--- LoginPage/createAccount.php
+++ LoginPage/createAccount.php
@@ -5,63 +5,103 @@ Date: 2016-12-16
 Email: fbastos1@umbc.edu, bac2@umbc.edu, te4@umbc.edu, fullern1@umbc.edu, cganley1@umbc.edu
 Users enter new account information using this sticky form. 
 -->
-
+<!DOCTYPE html>
+<meta charset="UTF-8">
 <html>
-  <head>
-    <title>Account Creation - UMBC CMNS Advising</title>
-    <style>
-        #preferred_name, #campusID, #major {
-            -webkit-transition: all 0.5s ease;
-            -moz-transition: all 0.5s ease;
-            transition: all 0.5s ease;
-            max-height: 50px;
-            overflow: hidden;
-        }
-        #advisor_rb:checked~#preferred_name, #advisor_rb:checked~#major  {
-            max-height: 0px;
-            opacity: 0;
-            pointer-events: none;      
-        }
-    </style>
-  </head>
-  <body>
+    <head>
+       <title>Create an account</title>
+		<link href="https://fonts.googleapis.com/css?family=Catamaran:300" rel="stylesheet">
+		<link href="../main.css" rel="stylesheet" type="text/css">
+    </head>
+  
+	<body>
+		<div id="wrapper">
+			<header>
+				<div id="Top-Header">
+					<div id="Page-Title">
+						<a class="Title">create an account</a>
+					</div>
+					<a href="http://umbc.edu"><img src="../umbc50.png" title="UMBC: An Honors University in Maryland" class="umbc-logo"></a>
+				</div>
+				<div id="Cnms-Banner">
+					<a href="http://cnms.umbc.edu"><img src="../cnms.png" class="banner"></a>
+				</div>
+			</header>
+			<form id="Main-Form" action='createAccount.php' method='post'>
+				<div class="Group-Appointment">
+					<a class="Descriptor">are you a student or advisor?</a>
+					<div id="RadialPanel">
+						<span class="RadialSelector">
+							<label class="RadialDescriptor" for="student_rb">student</label> <input id="student_rb" type="radio" name="userRole" value="student" checked>
+						</span>
+						<span class="RadialSelector">
+							<input type="radio" name="userRole" id="advisor_rb" value="advisor"> <label class="RadialDescriptor" for="advisor_rb">advisor</label>
+						</span>
+					</div>
+				</div>
+				
+				<input placeholder="First Name" class="inputField" type='varchar' size='10' maxlength='40' name='firstName' value="<?php if(isset($_POST['firstName'])) echo($_POST['firstName']); ?>" required>
+				<input placeholder="Last Name" class="inputField" type='varchar' size='10' maxlength='40' name='lastName' value="<?php if(isset($_POST['lastName'])) echo($_POST['lastName']); ?>" required>
+				<input placeholder="E-mail Address" class="inputField" type='email' name='email' size='15' placeholder="Ex: jDoe1@umbc.edu" value="<?php if(isset($_POST['email'])) echo($_POST['email']); ?>" required>
 
-    <h1>Create an account</h1>   
-    <form action='createAccount.php' method='post'>
-        <label><input type="radio" name="userRole" value="student" checked> Student </label>
-        <input type="radio" name="userRole" id="advisor_rb" value="advisor"><label for="advisor_rb">Advisor</label>
-        
-        <div> First Name: <input type='varchar' size='10' maxlength='40' name='firstName' value="<?php if(isset($_POST['firstName'])) echo($_POST['firstName']); ?>" required></div>
+				<input placeholder="Campus ID" class="inputField" type='varchar' size='7' maxlength='7' name='campusID' placeholder="Ex: AB12345" value="<?php if(isset($_POST['campusID'])) echo($_POST['campusID']); ?>" required>
 
-        <div> Last Name: <input type='varchar' size='10' maxlength='40' name='lastName' value="<?php if(isset($_POST['lastName'])) echo($_POST['lastName']); ?>" required></div>
+				<input placeholder="Password" class="inputField" type='password' name='password' size='10' maxlength='40' required>
+				<input placeholder="Confirm Password" class="inputField" type='password' name='confirmPass' size='10' maxlength='40' required>
 
-        <div> Email: <input type='email'name='email' size='15' placeholder="Ex: jDoe1@umbc.edu" value="<?php if(isset($_POST['email'])) echo($_POST['email']); ?>" required></div>
+				<div id="major">
+					<select name="major" class="LargeSelect">
+						<option value="Biological Sciences B.A.">Biological Sciences B.A.</option>
+						<option value="Biological Sciences B.S.">Biological Sciences B.S.</option>
+						<option value="Biochemistry & Molecular Biology B.S.">Biochemistry & Molecular Biology B.S.</option>
+						<option value="Bioinformatics & Computational Biology B.S.">Bioinformatics & Computational Biology B.S.</option>
+						<option value="Biology Education B.A.">Biology Education B.A.</option>	
+						<option value="Chemistry B.A.">Chemistry B.A.</option>
+						<option value="Chemistry B.S.">Chemistry B.S.</option>
+						<option value="Chemistry Education B.A.">Chemistry Education B.A.</option>
+						<option value="Other">Other</option>
+					</select>
+				</div>
 
-	<div id="campusID" > Campus ID: <input type='varchar' size='7' maxlength='7' name='campusID' placeholder="Ex: AB12345" value="<?php if(isset($_POST['campusID'])) echo($_POST['campusID']); ?>" required></div>
-
-        <div> Password: <input type='password' name='password' size='10' maxlength='40' required></div>
-
-        <div> Re-enter Password: <input type='password' name='confirmPass' size='10' maxlength='40' required></div>  
-
-        <div id="major">Major next semester: 
-            <select name="major">
-                <option value="Biological Sciences B.A.">Biological Sciences B.A.</option>
-                <option value="Biological Sciences B.S.">Biological Sciences B.S.</option>
-                <option value="Biochemistry & Molecular Biology B.S.">Biochemistry & Molecular Biology B.S.</option>
-                <option value="Bioinformatics & Computational Biology B.S.">Bioinformatics & Computational Biology B.S.</option>
-                <option value="Biology Education B.A.">Biology Education B.A.</option>
-                <option value="Chemistry B.A.">Chemistry B.A.</option>
-                <option value="Chemistry B.S.">Chemistry B.S.</option>
-                <option value="Chemistry Education B.A.">Chemistry Education B.A.</option>
-                <option value="Other">Other</option>
-            </select>
-        </div>
-
-        <div id="preferred_name" > Preferred Name: <input type='varchar' size='10' maxlength='40' name='preferredName' value="<?php if(isset($_POST['preferredName'])) echo($_POST['preferredName']); ?>" ></div>
-
-        <input type='submit' name='submit' value='Register'>
-    </form>
-  </body>
+				<input placeholder="Preferred Name" class="inputField" type='varchar' size='10' maxlength='40' name='preferredName' value="<?php if(isset($_POST['preferredName'])) echo($_POST['preferredName']); ?>" >
+				<div>
+					<button class="submit" id="Register" type='submit' name='submit'><span>register</span></button>
+				</div>
+			</form>
+			<div id="Inner-Footer">
+				<div class="main-inner-footer-field">College of Natural and Mathematical Sciences</div>
+				<div class="inner-footer-field">University Center Room 116</div>
+				<div class="inner-footer-field">(410) 455-5827</div>
+				<div class="inner-footer-field"><a class="inner-footer-link" href="mailto:cnms@umbc.edu">cnms@umbc.edu</a></div>
+			</div>
+		</div>
+		<div id="Footer">
+			<div>
+				<a href="http://umbc.edu"><img src="../footer.png" title="UMBC: An Honors University in Maryland" class="umbc-footer"></a>
+			</div>
+			<div>
+				<a href="http://about.umbc.edu">About UMBC</a> 
+				| 
+				<a href="http://about.umbc.edu/visitors-guide/contact-us">Contact Us</a> 
+				| 
+				<a href="http://umbc.edu/go/equal-opportunity">Equal Opportunity</a> 
+				| 
+				Follow UMBC:
+				<a href="https://facebook.com/umbcpage" title="Follow on Facebook">
+					<img class="facebook-footer" src="../fbft.png">
+				</a> 
+				<a href="https://twitter.com/umbc" title="Follow on Twitter">
+					<img class="twitter-footer" src="../twft.png">
+				</a> 
+				<a href="http://umbc.edu/news" title="UMBC News">
+					<img class="rss-footer" src="../rssft.png">
+				</a>
+			</div>
+			<div>
+				© University of Maryland, Baltimore County  •  1000 Hilltop Circle  •  Baltimore, MD 21250
+			</div>
+		</div>	
+	</body>
 </html>
 
 <?php
--- LoginPage/login.php
+++ LoginPage/login.php
@@ -4,39 +4,76 @@
    Date: 2016-12-13
    Email: fbastos1@umbc.edu, bac2@umbc.edu, te4@umbc.edu, fullern1@umbc.edu, cganley1@umbc.edu
 -->
+<!DOCTYPE html>
 
 <?php
 session_start();
 ?>
 
+<meta charset="UTF-8">
 <html>
-<head>
-  <title> Login </title>
-</head>
-<body>
+    <head>
+       <title>Welcome</title>
+		<link href="https://fonts.googleapis.com/css?family=Catamaran:300" rel="stylesheet">
+		<link href="../main.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+		<div id="wrapper">
+			<header>
+				<div id="Top-Header">
+					<div id="Page-Title">
+						<a class="Title">home page</a>
+					</div>
+					<a href="http://umbc.edu"><img src="../umbc50.png" title="UMBC: An Honors University in Maryland" class="umbc-logo"></a>
+				</div>
+				<div id="Cnms-Banner">
+					<a href="http://cnms.umbc.edu"><img src="../cnms.png" class="banner"></a>
+				</div>
+			</header>
 
-<form action='processLogin.php' method='post' name='UserLogin'>
-  <div class='field'>
-    <label for='username'>Camput ID:</label>
-    <input id='username' type='text' name='username' required>
-  </div>
-
-  <div class='field'>
-    <label for='password'>Password</label>
-    <input id='password' type='password' name='password' required>
-  </div>
-
-  <div class='loginButton'>
-    <input type='submit' value='Login'>
-  </div>
-</form>
-
-<form action = "createAccount.php" method="get">
-   <div class='field'>
-      <label for='submit'>Don't have an account set up?</label>
-      <input type='submit' value='Register'>
-  </div>
-</form>
-
-</body>
+		<div class="Restraint">
+			<form class="Main-Form" action="processLogin.php" method="post">
+				<input type="text" name="username" placeholder="Username" class="inputField">
+				<input type="password" name="password" placeholder="Password" class="inputField">
+				<div id="Buttons">
+					<button id="Login" class="submit"><span>login</span></button>
+				</div>
+			</form>	
+			<form class="Main-Form" action="createAccount.php"><button class="button"><span>create new account</span></button></form>
+		</div>
+		<div id="Inner-Footer">
+				<div class="main-inner-footer-field">College of Natural and Mathematical Sciences</div>
+				<div class="inner-footer-field">University Center Room 116</div>
+				<div class="inner-footer-field">(410) 455-5827</div>
+				<div class="inner-footer-field"><a class="inner-footer-link" href="mailto:cnms@umbc.edu">cnms@umbc.edu</a></div>
+			</div>
+		</div>
+		
+		<div id="Footer">
+			<div>
+				<a href="http://umbc.edu"><img src="../footer.png" title="UMBC: An Honors University in Maryland" class="umbc-footer"></a>
+			</div>
+			<div>
+				<a href="http://about.umbc.edu">About UMBC</a> 
+				| 
+				<a href="http://about.umbc.edu/visitors-guide/contact-us">Contact Us</a> 
+				| 
+				<a href="http://umbc.edu/go/equal-opportunity">Equal Opportunity</a> 
+				| 
+				Follow UMBC:
+				<a href="https://facebook.com/umbcpage" title="Follow on Facebook">
+					<img class="facebook-footer" src="../fbft.png">
+				</a> 
+				<a href="https://twitter.com/umbc" title="Follow on Twitter">
+					<img class="twitter-footer" src="../twft.png">
+				</a> 
+				<a href="http://umbc.edu/news" title="UMBC News">
+					<img class="rss-footer" src="../rssft.png">
+				</a>
+			</div>
+			<div>
+				© University of Maryland, Baltimore County  •  1000 Hilltop Circle  •  Baltimore, MD 21250
+			</div>
+		</div>		
+	</body>
 </html>
