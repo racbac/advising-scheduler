8bac46557e92ff9a80a292f93a73a78744bf4bb2
Rachel Backert <bac2@umbc.edu>
2016-12-18 12:19:13 -0500

    Frequent functions. Include to use.

---
+++ Utilities/phpFuns.php
@@ -0,0 +1,40 @@
+<?php
+
+    function dropAppt($student, $connect = false) {
+        if ($connect = false) {
+            include('../CommonMethods.php');
+            $connect = new Common(false);
+        }
+        // get curr appointment id
+        $sql = "SELECT `appointmentID` FROM `students_academic_info` WHERE `username` = '$student'";
+        $rs = $connect->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
+        $apptID = mysql_fetch_assoc($rs)['appointmentID'];
+        if ($apptID != NULL) {
+            // decrease number of students attending in appointments
+            $sql = "UPDATE `appointments` SET `curr_students` = `curr_students` - 1 WHERE `appointment_ID` = '$apptID'";
+            $rs = $connect->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
+            // set student's appointment to NULL
+            $sql = "UPDATE `students_academic_info` SET `appointmentID` = NULL WHERE `username` = '$student'";
+            $rs = $connect->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
+        }
+        unset($connect);
+    }
+
+    function sticky($name, $default = false) {
+        if(isset($_POST[$name])) 
+            echo(" value=".$_POST[$name]); 
+        else if ($default != false)
+            echo(" value=".$default);
+    }
+    function stickySelect($name, $value, $default) {
+        if(isset($_POST[$name])) {
+            if ($_POST[$name] == $value) { 
+                echo(" selected"); 
+            }
+        }
+        else if ($value == $default) {
+            echo(" selected");
+        }
+    }
+
+?>
