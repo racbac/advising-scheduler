e03117aad6c094384e3684e60ca77170c1fcb47b
Felipe Bastos <felipe.bastos@terra.com.br>
2016-12-19 18:06:54 -0500

    Styling changes to awayPage.php

--- LoginPage/awayPage.php
+++ LoginPage/awayPage.php
@@ -1,6 +1,79 @@
+<!--htmlHeader
+   Project: CMSC331 Project 02, Fall 2016
+   Authors: Felipe Bastos, Rachel Brackert, Travis Early, Nathaniel Fuller, Colin Ganley
+   Date: 2016-12-13
+   Email: fbastos1@umbc.edu, bac2@umbc.edu, te4@umbc.edu, fullern1@umbc.edu, cganley1@umbc.edu
+-->
+<!DOCTYPE html>
+
 <?php
-
-
-echo "Offseason, please come back during advising season";
-
+session_start();
 ?>
+
+<meta charset="UTF-8">
+<html>
+    <head>
+       <title>Welcome</title>
+   		<link rel="icon" type="image/png" href="http://sites.umbc.edu/wp-content/themes/umbc/assets/images/icon.png">
+		<link href="https://fonts.googleapis.com/css?family=Catamaran:300" rel="stylesheet">
+		<link href="../main.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+		<div id="wrapper">
+			<header>
+				<div id="Top-Header">
+					<div id="Page-Title">
+						<a class="Title">advising is closed</a>
+					</div>
+					<a href="http://umbc.edu"><img src="../umbc50.png" title="UMBC: An Honors University in Maryland" class="umbc-logo"></a>
+				</div>
+				<div id="Cnms-Banner">
+					<a href="http://cnms.umbc.edu"><img src="../cnms.png" class="banner"></a>
+				</div>
+			</header>
+
+			<div class="Main-Form">
+				<div class='ErrorDiv'>
+					<div class='InnerErrorDiv'>
+						<a class='ErrorBackground'>error</a>
+						<a class='Error'>Advising sign-ups are currently closed. Please check back later</a>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div id="Inner-Footer">
+				<div class="main-inner-footer-field">College of Natural and Mathematical Sciences</div>
+				<div class="inner-footer-field">University Center Room 116</div>
+				<div class="inner-footer-field">(410) 455-5827</div>
+				<div class="inner-footer-field"><a class="inner-footer-link" href="mailto:cnms@umbc.edu">cnms@umbc.edu</a></div>
+			</div>
+		</div>
+		
+		<div id="Footer">
+			<div>
+				<a href="http://umbc.edu"><img src="../footer.png" title="UMBC: An Honors University in Maryland" class="umbc-footer"></a>
+			</div>
+			<div>
+				<a href="http://about.umbc.edu">About UMBC</a> 
+				| 
+				<a href="http://about.umbc.edu/visitors-guide/contact-us">Contact Us</a> 
+				| 
+				<a href="http://umbc.edu/go/equal-opportunity">Equal Opportunity</a> 
+				| 
+				Follow UMBC:
+				<a href="https://facebook.com/umbcpage" title="Follow on Facebook">
+					<img class="facebook-footer" src="../fbft.png">
+				</a> 
+				<a href="https://twitter.com/umbc" title="Follow on Twitter">
+					<img class="twitter-footer" src="../twft.png">
+				</a> 
+				<a href="http://umbc.edu/news" title="UMBC News">
+					<img class="rss-footer" src="../rssft.png">
+				</a>
+			</div>
+			<div>
+				© University of Maryland, Baltimore County  •  1000 Hilltop Circle  •  Baltimore, MD 21250
+			</div>
+		</div>		
+	</body>
+</html>
